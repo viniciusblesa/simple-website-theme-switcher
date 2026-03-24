e2a9ca2f8092d825134b98096906c39901c617d5
Vinícius Blesa dos Santos <blesa1269@gmail.com>
2026-03-24 14:57:21 -0300

    refactor: melhora lógica de alternância de tema

--- app.js
+++ app.js
@@ -5,12 +5,27 @@ const switcher = document.querySelector('.btn');
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme')
 
-    var className = document.body.className;
-    if(className == "light-theme"){
+    if(document.body.classList.contains('dark-theme')){
+        this.textContent = "Light";
+    } else{
         this.textContent = "Dark";
     }
-    else{
+    if(className == "light-theme"){
+        this.textContent = "Dark";
+    } else{
         this.textContent = "light";
+    }
+
+    if(document.body.classList.contains('dark-theme')){
+        localStoarge.setItem('theme', 'dark');
+    } else{
+        localStorage.setItem('theme', 'light');
+    }
+
+    const temaSalvo = localStorage.getItem('theme');
+
+    if (temaSalvo === 'dark'){
+        document.body.classList.add('dark-theme');
     }
 
     console.log('current class name: ' + className);
